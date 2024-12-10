2d4c1624ab73da1a674e3010c1ab712e82e26810
weston-2222 <westonxie22@gmail.com>
2024-12-10 23:13:31 +0800

    feat: 新增幫數字加上逗號的函數

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+// 將數字格式加上逗號
+export function formatNumberWithCommas(price: number): string {
+  return new Intl.NumberFormat('en-US').format(price);
 }
