88179e2b51da260f87b17cf592a73ea6c6ef959e
weston-2222 <westonxie22@gmail.com>
2024-12-13 19:13:57 +0800

    feat: 增加將價格格式化的工具函數

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,3 +9,18 @@ export function cn(...inputs: ClassValue[]) {
 export function formatNumberWithCommas(price: number): string {
   return new Intl.NumberFormat('en-US').format(price);
 }
+
+//將數字轉換成文字
+export function formatPrice(price: number): string {
+  if (price >= 1000000000000) {
+    return 'US$' + (price / 1000000000000).toFixed(2) + '兆';
+  } else if (price >= 100000000) {
+    return 'US$' + (price / 100000000).toFixed(1) + '億';
+  } else if (price >= 10000) {
+    return 'US$' + (price / 10000).toFixed(1) + '萬';
+  } else if (price >= 1000) {
+    return 'US$' + Math.floor(price).toString();
+  }
+
+  return new Intl.NumberFormat('en-US').format(price);
+}
