b3a7086029a4eec817ee4fe5a2f2f44d2ed6729e
weston-2222 <westonxie22@gmail.com>
2024-12-13 19:18:14 +0800

    feat: 將page的元件分離出來

---
+++ src/app/coin/[id]/coinPriceTabs.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import TradingViewWidget from '@/dynamic/dynamicTradingViewWidget';
+
+import PricesCharts from '@/components/pricesCharts';
+import { useState } from 'react';
+
+const CoinPriceTabs = ({
+  coinId,
+  symbol,
+}: {
+  coinId: string;
+  symbol: string;
+}) => {
+  const [type, setType] = useState<'price' | 'market_caps'>('price');
+  return (
+    <Tabs
+      defaultValue='prices'
+      className='w-full h-full'
+      onValueChange={(value) => setType(value as 'price' | 'market_caps')}
+    >
+      <TabsList>
+        <TabsTrigger value='price'>Prices</TabsTrigger>
+        <TabsTrigger value='market_caps'>Market Caps</TabsTrigger>
+        <TabsTrigger value='TradingView'>TradingView</TabsTrigger>
+      </TabsList>
+
+      <TabsContent value='TradingView' className='h-full w-full'>
+        <TradingViewWidget symbol={symbol} />
+      </TabsContent>
+
+      {/* <TabsContent value={type} className='h-full w-full'> */}
+      <PricesCharts coinId={coinId} type={type} />
+      {/* </TabsContent> */}
+      {/* 
+      <TabsContent value='market_caps' className='h-full w-full'>
+        <p>Change your password here.</p>
+      </TabsContent> */}
+    </Tabs>
+  );
+};
+
+export default CoinPriceTabs;
