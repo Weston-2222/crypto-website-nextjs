93c602f9375ce6347eaf87d60acc13225a247eb8
weston-2222 <westonxie22@gmail.com>
2024-12-17 01:58:10 +0800

    feat: 新增coin頁面的loading

---
+++ src/app/coin/[id]/loading.tsx
@@ -0,0 +1,10 @@
+// 這裡是你的加載內容
+const Loading = () => {
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <div className='loading-spinner w-20 h-20'></div>
+    </div>
+  );
+};
+
+export default Loading;
