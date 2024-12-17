bf9d065a00814412fe744d412299427f76a8896c
weston-2222 <westonxie22@gmail.com>
2024-12-17 22:59:31 +0800

    feat: 增加loading元件功能
    多兩個參數可以自定義className

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,7 +1,17 @@
 import { cn } from '@/lib/utils';
 
-const Loading = ({ className }: { className?: string }) => {
-  return <div className={cn('loading-spinner', className)}></div>;
+const Loading = ({
+  className,
+  loadingSize,
+}: {
+  className?: string;
+  loadingSize?: string;
+}) => {
+  return (
+    <div className={cn('flex justify-center items-center', className)}>
+      <div className={cn('loading-spinner', loadingSize)}></div>
+    </div>
+  );
 };
 
 export default Loading;
