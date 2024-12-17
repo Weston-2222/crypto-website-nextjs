a6aba1a7685c829778ca06855a9b211fbf83d0a8
weston-2222 <westonxie22@gmail.com>
2024-12-17 23:42:44 +0800

    feat: 將元件分離並加上loading動畫

--- src/app/coin/[id]/page.tsx
+++ src/app/coin/[id]/page.tsx
@@ -1,50 +1,78 @@
 import 'server-only';
 import CoinTitle from '@/components/coinTitle';
-
-import { CoinInfoApiResponse } from '@/types/api/coingecko/coinInfo';
-import CoinPriceTabs from './coinPriceTabs';
-import { CoinsMarketsApiResponse } from '@/types/api/coingecko/coinsMarkets';
-import InfoCard, { InfoCardConfig } from '@/components/infoCard';
-
-import {
-  getDeveloperInfoConfig,
-  getLinkInfoConfig,
-  getMerketInfoConfig,
-} from './contentConfig';
-import { fetchCoinData, fetchCoinMarketData } from '@/services/coin/coinGecko';
+import { Suspense } from 'react';
+import Mymotion from '../../../components/myLoadingMotion';
+import MarketInfo from './components/marketInfo';
+import Loading from '@/components/loading';
+import LinkInfo from './components/linkInfo';
+import DeveloperInfo from './components/developerInfo';
+import CategoryInfo from './components/categoryInfo';
+import PriceChart from './components/priceChart';
 
 const CoinPage = async ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = await params;
 
-  // 取得幣種資料
-  const coinData: CoinInfoApiResponse = await fetchCoinData(id);
-  // 取得幣種市場資料
-  const coinMarketData: CoinsMarketsApiResponse = await fetchCoinMarketData(id);
-
-  // 取得幣種市場資料設定
-  const marketInfoConfig: InfoCardConfig = getMerketInfoConfig(coinMarketData);
-  // 取得幣種開發者資料設定
-  const developerInfoConfig: InfoCardConfig = getDeveloperInfoConfig(coinData);
-  // 取得幣種鏈接資料設定
-  const linkInfoConfig: InfoCardConfig = getLinkInfoConfig(coinData);
-
   return (
     <>
       <div className='w-full'>
-        <CoinTitle id={id} />
+        <div>
+          <Suspense fallback={<Loading loadingSize='w-20 h-20' />}>
+            <Mymotion>
+              <CoinTitle id={id} />
+            </Mymotion>
+          </Suspense>
+        </div>
       </div>
 
       <div className='flex flex-col xl:flex-row lg:space-x-4 space-y-4 xl:space-y-0 gap-4'>
         {/* 在中小螢幕時 CoinPriceTabs 在上面，正常電腦螢幕時 CoinPriceTabs 在右邊 */}
         <div className='xl:order-2 w-full xl:w-2/3'>
-          <CoinPriceTabs coinId={id} symbol={coinData.symbol} />
+          <Suspense
+            fallback={<Loading className='h-1/2' loadingSize='w-20 h-20' />}
+          >
+            <Mymotion>
+              <PriceChart id={id} />
+            </Mymotion>
+          </Suspense>
+
+          {/* 市場資料 */}
+          <Suspense
+            fallback={<Loading className='h-1/2' loadingSize='w-20 h-20' />}
+          >
+            <Mymotion>
+              <CategoryInfo id={id} />
+            </Mymotion>
+          </Suspense>
         </div>
 
         {/* 在中小螢幕時 CoinBasicInfoCard 在下面，正常電腦螢幕時 CoinBasicInfoCard 在左邊 */}
         <div className='xl:order-1 w-full xl:w-1/3'>
-          <InfoCard config={marketInfoConfig} />
-          <InfoCard config={linkInfoConfig} />
-          <InfoCard config={developerInfoConfig} />
+          {/* 市場資料 */}
+          <Suspense
+            fallback={<Loading className='h-1/2' loadingSize='w-20 h-20' />}
+          >
+            <Mymotion>
+              <MarketInfo id={id} />
+            </Mymotion>
+          </Suspense>
+
+          {/* 鏈接資料 */}
+          <Suspense
+            fallback={<Loading className='h-1/2' loadingSize='w-20 h-20' />}
+          >
+            <Mymotion>
+              <LinkInfo id={id} />
+            </Mymotion>
+          </Suspense>
+
+          {/* 開發者資料 */}
+          <Suspense
+            fallback={<Loading className='h-1/2' loadingSize='w-20 h-20' />}
+          >
+            <Mymotion>
+              <DeveloperInfo id={id} />
+            </Mymotion>
+          </Suspense>
         </div>
       </div>
     </>
