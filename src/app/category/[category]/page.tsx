8b5bc15ed58e8b9ce6c9793e5f0ec2d2a204c5c4
weston-2222 <westonxie22@gmail.com>
2024-12-17 23:47:59 +0800

    fix: 修正參數接受方式

--- src/app/category/[category]/page.tsx
+++ src/app/category/[category]/page.tsx
@@ -1,14 +1,28 @@
+import 'server-only';
 import CoinMasketsCapTable from '@/components/coinMasketsCapTable';
-import { fetchCoinMarketData } from '@/services/coin/coinGecko';
+import {
+  fetchCoinCategoryList,
+  fetchCoinMarketData,
+} from '@/services/coin/coinGecko';
 import { CoinsMarketsApiResponse } from '@/types/api/coingecko/coinsMarkets';
 
-const page = async ({ params }: { params: { category: string } }) => {
+const page = async ({ params }: { params: Promise<{ category: string }> }) => {
   const { category } = await params;
+
   const data: CoinsMarketsApiResponse[] = await fetchCoinMarketData({
     category,
   });
+  const categoryList = await fetchCoinCategoryList();
+  const categoryName = categoryList.find(
+    (item) => item.category_id === category
+  )?.name;
 
-  return <CoinMasketsCapTable data={data} />;
+  return (
+    <div className='py-10'>
+      <h1 className='text-3xl p-5'>{categoryName}類別市值排名</h1>
+      <CoinMasketsCapTable data={data} />
+    </div>
+  );
 };
 
 export default page;
