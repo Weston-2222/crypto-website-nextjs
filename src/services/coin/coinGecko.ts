2b61e215367c5f56bea90fadd29a542825b3e27a
weston-2222 <westonxie22@gmail.com>
2024-12-17 23:00:12 +0800

    feat: 增加api

--- src/services/coin/coinGecko.ts
+++ src/services/coin/coinGecko.ts
@@ -1,3 +1,5 @@
+import { CoinCategoriesListApiResponse } from '@/types/api/coingecko/categoriesList';
+import { CoinCategoriesApiResponse } from '@/types/api/coingecko/coinCategories';
 import { CoinInfoApiResponse } from '@/types/api/coingecko/coinInfo';
 import { CoinsMarketsApiResponse } from '@/types/api/coingecko/coinsMarkets';
 
@@ -28,26 +30,61 @@ export const fetchCoinData = async (
     } as CoinInfoApiResponse;
   }
 };
-export const fetchCoinMarketData = async (
-  id: string
-): Promise<CoinsMarketsApiResponse> => {
-  try {
+// 獲取市場資料
+export const fetchCoinMarketData = async (params: {
+  category?: string;
+  id?: string;
+}): Promise<CoinsMarketsApiResponse[]> => {
+  const url = new URL(`${process.env.COINGECKO_API_URL}/coins/markets`);
+
+  url.searchParams.append('vs_currency', 'usd');
+  Object.entries(params).forEach(([key, value]) => {
+    url.searchParams.append(key, value);
+  });
+
+  const data = await fetch(url.toString(), {
+    headers: {
+      accept: 'application/json',
+      'x-cg-demo-api-key': process.env.COINGECKO_API_KEY || '',
+    },
+    next: { revalidate: 60 },
+  });
+  const newData = await data.json();
+  return newData.map((item: CoinsMarketsApiResponse) => ({
+    ...item,
+    name_symbol: `${item.name}_${item.symbol}`,
+  }));
+};
+/**
+ * 獲取所有類別的數據
+ * @returns
+ */
+export const fetchCoinCategoriesData = async (): Promise<
+  CoinCategoriesApiResponse[]
+> => {
+  const data = await fetch(
+    `${process.env.COINGECKO_API_URL}/coins/categories`,
+    {
+      headers: {
+        accept: 'application/json',
+        'x-cg-demo-api-key': process.env.COINGECKO_API_KEY || '',
+      },
+    }
+  );
+  return await data.json();
+};
+
+// 獲取所有類別list
+export const fetchCoinCategoryList =
+  async (): Promise<CoinCategoriesListApiResponse> => {
     const data = await fetch(
-      `${process.env.COINGECKO_API_URL}/coins/markets?ids=${id}&vs_currency=usd`,
+      `${process.env.COINGECKO_API_URL}/coins/categories/list`,
       {
         headers: {
           accept: 'application/json',
           'x-cg-demo-api-key': process.env.COINGECKO_API_KEY || '',
         },
-        next: { revalidate: 60 },
       }
     );
-    if (!data.ok) {
-      throw new Error(`Error fetching data: ${data.statusText}`);
-    }
-    return (await data.json())[0];
-  } catch (error) {
-    console.error('Failed to fetch coin data:', error);
-    return {} as CoinsMarketsApiResponse;
-  }
-};
+    return await data.json();
+  };
