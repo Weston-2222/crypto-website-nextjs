7dae221953e5c524ba3a627838b73e3345f79f1d
weston-2222 <westonxie22@gmail.com>
2024-12-17 23:36:33 +0800

    feat: 增加openPage參數

--- src/components/myHoverCardButton.tsx
+++ src/components/myHoverCardButton.tsx
@@ -2,12 +2,14 @@
 import 'client-only';
 import { HoverCard, HoverCardContent, HoverCardTrigger } from './ui/hover-card';
 import { cn } from '@/lib/utils';
+import { useRouter } from 'next/navigation';
 interface ButtonProps {
   children: React.ReactNode; // 按鈕內容
   onClick?: () => void; // 點擊事件處理
   className?: string; // 自定義類名
   hoverCardContent?: string;
-  openLink?: string; // 打開的連結
+  openLink?: string; // 打開連結
+  openPage?: string; // 打開頁面
 }
 const MyHoverCardButton: React.FC<ButtonProps> = ({
   children,
@@ -15,13 +17,18 @@ const MyHoverCardButton: React.FC<ButtonProps> = ({
   className,
   hoverCardContent,
   openLink,
+  openPage,
 }) => {
+  const router = useRouter();
   const tailwindCss = className
     ? className
     : 'flex items-center hover:cursor-pointer p-1 px-4 bg-gray-800 text-white rounded-md hover:bg-gray-100 hover:text-black transition duration-200';
   const handleClick = () => {
     if (openLink) {
       window.open(openLink, '_blank', 'noreferrer noopener');
+    }
+    if (openPage) {
+      router.push(openPage);
     }
     if (onClick) {
       onClick();
