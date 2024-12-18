97860ce6ba2fbf3993fffbcb049b9ea804fd003d
weston-2222 <westonxie22@gmail.com>
2024-12-19 01:39:02 +0800

    feat: 建立NextAuth設定

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,38 @@
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+import { authorize } from './authorize';
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'text',
+          placeholder: 'email@example.com',
+        },
+        password: { label: 'Password', type: 'password' },
+      },
+      authorize: authorize,
+    }),
+  ],
+
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+      }
+      return token;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
