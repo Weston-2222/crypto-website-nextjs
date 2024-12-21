a3d938aff8894545f0b0fd340cbb68a4c79ce7c7
weston-2222 <westonxie22@gmail.com>
2024-12-22 05:13:55 +0800

    feat: 修改元件的輸入參數

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,18 @@
 import 'server-only';
-
-import { CoinsMarketsApiResponse } from '@/types/api/coingecko/coinsMarkets';
 import React from 'react';
 import CoinMasketsCapTable from '../components/coinMasketsCapTable';
+import { getCoinMarketsData } from '@/services/coinGecko/coinMarkets';
 
 export const revalidate = 3600; // 每小時重新生成靜態頁面（3600 秒）
 
 const Page = async () => {
   try {
-    const res = await fetch(
-      `${process.env.COINGECKO_API_URL}/coins/markets?vs_currency=usd`,
-      {
-        headers: {
-          'X-CMC_PRO_API_KEY': process.env.COINGECKO_API_KEY || '',
-          accept: 'application/json',
-        },
-        next: { revalidate }, // 啟用 ISR
-      }
-    );
-
-    if (!res.ok) {
-      throw new Error(`HTTP 錯誤！狀態碼：${res.status}`);
-    }
-
-    const data: CoinsMarketsApiResponse[] = await res.json();
-
-    const nameAndSymbol = data.map((item) => ({
-      ...item,
-      name_symbol: `${item.name}_${item.symbol}`,
-    }));
+    const data = await getCoinMarketsData();
 
     return (
       <div className='container mx-auto p-10'>
         <h1 className='text-3xl p-5'>市值排名</h1>
-        <CoinMasketsCapTable data={nameAndSymbol} />
+        <CoinMasketsCapTable data={data} />
       </div>
     );
   } catch {
