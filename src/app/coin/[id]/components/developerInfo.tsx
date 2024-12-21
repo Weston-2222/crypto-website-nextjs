655194f210bc318cfa25db71790538386cf5d454
weston-2222 <westonxie22@gmail.com>
2024-12-22 05:09:49 +0800

    feat: 新增錯誤處理

--- src/app/coin/[id]/components/developerInfo.tsx
+++ src/app/coin/[id]/components/developerInfo.tsx
@@ -1,17 +1,22 @@
 import InfoCard, { InfoCardConfig } from '@/components/infoCard';
-import { fetchCoinData } from '@/services/coin/coinGecko';
+import { getCoinBasicData } from '@/services/coinGecko/coinBasic';
 import { CoinInfoApiResponse } from '@/types/api/coingecko/coinInfo';
 import 'server-only';
 import { getDeveloperInfoConfig } from '../contentConfig';
 
 const DeveloperInfo = async ({ id }: { id: string }) => {
   // 取得幣種資料
-  const coinData: CoinInfoApiResponse = await fetchCoinData(id);
+  try {
+    const coinData: CoinInfoApiResponse = await getCoinBasicData(id);
 
-  // 取得幣種開發者資料設定
-  const developerInfoConfig: InfoCardConfig = getDeveloperInfoConfig(coinData);
+    // 取得幣種開發者資料設定
+    const developerInfoConfig: InfoCardConfig =
+      getDeveloperInfoConfig(coinData);
 
-  return <InfoCard config={developerInfoConfig} />;
+    return <InfoCard config={developerInfoConfig} />;
+  } catch {
+    return <p>無法取得開發者資料</p>;
+  }
 };
 
 export default DeveloperInfo;
