939016308636cf977906eb5f51cd889d587160c9
weston-2222 <westonxie22@gmail.com>
2024-12-22 05:05:15 +0800

    feat: 修改icon

--- src/components/themeToggle.tsx
+++ src/components/themeToggle.tsx
@@ -3,6 +3,7 @@
 import { useTheme } from 'next-themes';
 
 import { Button } from '@/components/ui/button';
+import { IconMoon, IconSun } from '@tabler/icons-react';
 
 export function ThemeToggle() {
   const { theme, setTheme } = useTheme();
@@ -12,7 +13,11 @@ export function ThemeToggle() {
       variant='outline'
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
     >
-      Button
+      {theme === 'dark' ? (
+        <IconSun className='h-4 w-4' />
+      ) : (
+        <IconMoon className='h-4 w-4' />
+      )}
     </Button>
   );
 }
