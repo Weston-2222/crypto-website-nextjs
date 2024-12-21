ecd7328b881fc165da34c29b85eb81543306a818
weston-2222 <westonxie22@gmail.com>
2024-12-22 05:09:57 +0800

    feat: 新增錯誤處理

--- src/app/coin/[id]/components/marketInfo.tsx
+++ src/app/coin/[id]/components/marketInfo.tsx
@@ -1,20 +1,24 @@
 import 'server-only';
 import InfoCard, { InfoCardConfig } from '@/components/infoCard';
-import { fetchCoinMarketData } from '@/services/coin/coinGecko';
+import { getCoinMarketsData } from '@/services/coinGecko/coinMarkets';
 import { CoinsMarketsApiResponse } from '@/types/api/coingecko/coinsMarkets';
 import { getMerketInfoConfig } from '../contentConfig';
 
 const MarketInfo = async ({ id }: { id: string }) => {
   // 取得幣種市場資料
-  const coinMarketData: CoinsMarketsApiResponse[] = await fetchCoinMarketData({
-    id,
-  });
+  try {
+    const coinMarketData: CoinsMarketsApiResponse[] = await getCoinMarketsData({
+      ids: [id],
+    });
 
-  // 取得幣種市場資料設定
-  const marketInfoConfig: InfoCardConfig = getMerketInfoConfig(
-    coinMarketData[0]
-  );
-  return <InfoCard config={marketInfoConfig} />;
+    // 取得幣種市場資料設定
+    const marketInfoConfig: InfoCardConfig = getMerketInfoConfig(
+      coinMarketData[0]
+    );
+    return <InfoCard config={marketInfoConfig} />;
+  } catch {
+    return <p>無法取得市場資料</p>;
+  }
 };
 
 export default MarketInfo;
