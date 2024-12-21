69acef072f715010c2ec322ec84179ca81a692fe
weston-2222 <westonxie22@gmail.com>
2024-12-22 05:10:00 +0800

    feat: 新增錯誤處理

--- src/app/coin/[id]/components/linkInfo.tsx
+++ src/app/coin/[id]/components/linkInfo.tsx
@@ -1,16 +1,21 @@
 import 'server-only';
 import InfoCard, { InfoCardConfig } from '@/components/infoCard';
-import { fetchCoinData } from '@/services/coin/coinGecko';
+import { getCoinBasicData } from '@/services/coinGecko/coinBasic';
 import { CoinInfoApiResponse } from '@/types/api/coingecko/coinInfo';
 import { getLinkInfoConfig } from '../contentConfig';
 
 const LinkInfo = async ({ id }: { id: string }) => {
   // 取得幣種資料
-  const coinData: CoinInfoApiResponse = await fetchCoinData(id);
-  // 取得幣種鏈接資料設定
-  const linkInfoConfig: InfoCardConfig = getLinkInfoConfig(coinData);
+  try {
+    const coinData: CoinInfoApiResponse = await getCoinBasicData(id);
 
-  return <InfoCard config={linkInfoConfig} />;
+    // 取得幣種鏈接資料設定
+    const linkInfoConfig: InfoCardConfig = getLinkInfoConfig(coinData);
+
+    return <InfoCard config={linkInfoConfig} />;
+  } catch {
+    return <p>無法取得鏈接資料</p>;
+  }
 };
 
 export default LinkInfo;
