4cdf84d1398add246eb167a836e7096e415b1222
weston-2222 <westonxie22@gmail.com>
2024-12-22 05:01:40 +0800

    fix: 將元件分離成兩個檔案

--- src/app/(user)/profile/favoriteCoins.tsx
+++ src/app/(user)/profile/favoriteCoins.tsx
@@ -1,21 +1,50 @@
-'use client';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import 'server-only';
+import ReloadButton from './reloadButton';
 
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { connectToDB } from '@/lib/mongodb';
+import User from '@/models/user';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import { getCoinMarketsData } from '@/services/coinGecko/coinMarkets';
+import CoinMasketsCapTable from '@/components/coinMasketsCapTable';
+import { CoinsMarketsApiResponse } from '@/types/api/coingecko/coinsMarkets';
 
-const FavoriteCoins = ({ children }: { children: React.ReactNode }) => {
-  const router = useRouter();
-  useEffect(() => {
-    handleReload();
-  }, []);
-  const handleReload = () => {
-    router.refresh();
-  };
+const FavoriteCoins = async () => {
+  const session = await getServerSession(authOptions);
+  await connectToDB();
+  const user = await User.findById(session?.user._id);
+  const coinIds = user?.favorites.coins.reduce((arr, coin) => {
+    arr.push(coin.coin_id);
+    return arr;
+  }, [] as string[]);
+  let data: CoinsMarketsApiResponse[] | null = null;
+  if (coinIds && coinIds.length > 0) {
+    data = await getCoinMarketsData({
+      ids: coinIds,
+    });
+  }
+
   return (
-    <>
-      <button onClick={handleReload}>reload</button>
-      {children}
-    </>
+    <Card>
+      <CardHeader>
+        <CardTitle>收藏的幣種</CardTitle>
+        <CardDescription>收藏的幣種</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ReloadButton />
+        {data ? <CoinMasketsCapTable data={data} /> : <p>沒有收藏任何幣種</p>}
+      </CardContent>
+      {/* <CardFooter>
+    <p></p>
+  </CardFooter> */}
+    </Card>
   );
 };
 
--- src/app/(user)/profile/page.tsx
+++ src/app/(user)/profile/page.tsx
@@ -1,55 +1,7 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
 import FavoriteCoins from './favoriteCoins';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import { CoinsMarketsApiResponse } from '@/types/api/coingecko/coinsMarkets';
-import CoinMasketsCapTable from '@/components/coinMasketsCapTable';
-import { connectToDB } from '@/lib/mongodb';
-import User from '@/models/user';
-export const dynamic = 'force-dynamic';
 
 const Page = async () => {
-  const session = await getServerSession(authOptions);
-  await connectToDB();
-  const user = await User.findOne({ _id: session?.user._id });
-  const coinIds = user?.favorites.coins.map((coin) => coin.coin_id).join(',');
-  const url = new URL(`${process.env.COINGECKO_API_URL}/coins/markets`);
-  url.searchParams.append('vs_currency', 'usd');
-
-  if (!coinIds) return <></>;
-  url.searchParams.append('ids', coinIds);
-
-  const favoriteCoinsDataResponse = await fetch(url.toString(), {
-    headers: {
-      'X-CMC_PRO_API_KEY': process.env.COINGECKO_API_KEY || '',
-      accept: 'application/json',
-    },
-  });
-  const favoriteCoinsData: CoinsMarketsApiResponse[] =
-    await favoriteCoinsDataResponse.json();
-
-  return (
-    <Card>
-      <CardHeader>
-        <CardTitle>收藏的幣種</CardTitle>
-        <CardDescription>收藏的幣種</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <FavoriteCoins>
-          <CoinMasketsCapTable data={favoriteCoinsData} />
-        </FavoriteCoins>
-      </CardContent>
-      {/* <CardFooter>
-        <p></p>
-      </CardFooter> */}
-    </Card>
-  );
+  return <FavoriteCoins />;
 };
 
 export default Page;
