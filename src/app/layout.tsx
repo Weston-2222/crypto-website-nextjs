a0f665b4857f1489ecef277dc6c973b4edfdc195
weston-2222 <westonxie22@gmail.com>
2024-12-22 05:03:17 +0800

    refactor: 將sidebar分離

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,134 +1,38 @@
 'use client';
-import React, { useState } from 'react';
-import { Sidebar, SidebarBody, SidebarLink } from '@/components/ui/sidebar';
-import { IconBrandTabler, IconUserBolt } from '@tabler/icons-react';
-import Link from 'next/link';
-import { motion } from 'framer-motion';
-import Image from 'next/image';
-import { cn } from '@/lib/utils';
 import './globals.css';
+import { cn } from '@/lib/utils';
 import { ThemeProvider } from '@/components/theme-provider';
-import { ThemeToggle } from '@/components/themeToggle';
+import { SessionProvider } from 'next-auth/react';
+import MySidebar from './mySidebar';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  const links = [
-    {
-      label: '市值排行',
-      href: '/',
-      icon: (
-        <IconBrandTabler className='text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0' />
-      ),
-    },
-    {
-      label: '分類',
-      href: '/category',
-      icon: (
-        <IconUserBolt className='text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0' />
-      ),
-    },
-  ];
-  const [open, setOpen] = useState(false);
   return (
     <html lang='zh-TW' className='dark'>
       <body>
-        <ThemeProvider attribute='class' defaultTheme='dark'>
-          {/* // enableSystem
-          // disableTransitionOnChange */}
-          <div
-            className={cn(
-              'rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1  mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden h-screen'
-            )}
-            style={{ overflowY: 'auto' }}
-          >
-            <Sidebar open={open} setOpen={setOpen} animate={false}>
-              <SidebarBody className='justify-between gap-10'>
-                <div className='flex flex-col flex-1 overflow-y-auto overflow-x-hidden'>
-                  <>
-                    <Logo />
-                  </>
-                  <div className='mt-8 flex flex-col gap-2'>
-                    {links.map((link, idx) => (
-                      <SidebarLink key={idx} link={link} />
-                    ))}
-                  </div>
-                </div>
-                <div>
-                  <SidebarLink
-                    link={{
-                      label: 'Manu Arora',
-                      href: '#',
-                      icon: (
-                        <Image
-                          src='https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400'
-                          className='h-7 w-7 flex-shrink-0 rounded-full'
-                          width={50}
-                          height={50}
-                          alt='Avatar'
-                        />
-                      ),
-                    }}
-                  />
-                  <ThemeToggle />
-                </div>
-              </SidebarBody>
-            </Sidebar>
-
-            <Dashboard>{children}</Dashboard>
-          </div>{' '}
-        </ThemeProvider>
+        <SessionProvider>
+          <ThemeProvider attribute='class' defaultTheme='dark'>
+            <div
+              className={cn(
+                'rounded-md flex flex-col md:flex-row bg-gray-100 dark:bg-neutral-800 w-full flex-1  mx-auto border border-neutral-200 dark:border-neutral-700 overflow-hidden h-screen'
+              )}
+              style={{ overflowY: 'auto' }}
+            >
+              <MySidebar />
+              <Dashboard>{children}</Dashboard>
+            </div>
+          </ThemeProvider>
+        </SessionProvider>
       </body>
     </html>
   );
 }
-const Logo = () => {
-  return (
-    <Link
-      href='#'
-      className='font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20'
-    >
-      <div className='h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0' />
-      <motion.span
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        className='font-medium text-black dark:text-white whitespace-pre'
-      >
-        Acet Labs
-      </motion.span>
-    </Link>
-  );
-};
-// const LogoIcon = () => {
-//   return (
-//     <Link
-//       href='#'
-//       className='font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20'
-//     >
-//       <div className='h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0' />
-//     </Link>
-//   );
-// };
 
-// Dummy dashboard component with content
 const Dashboard = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className='flex flex-1 '>
       <div className='overflow-y-auto p-2 md:p-5 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-white dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full '>
-        <div className='gap-2'>
-          {/* {[...new Array(4)].map((i) => (
-            <div
-              key={'first' + i}
-              className='h-200 w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse'
-            ></div>
-          ))} */}
-          {children}
-        </div>
-        <div className='flex gap-2 flex-1'>
-          {/* {[...new Array(2)].map((i) => (
-            <div
-              key={'second' + i}
-              className='h-full w-full rounded-lg  bg-gray-100 dark:bg-neutral-800 animate-pulse'
-            ></div>
-          ))} */}
-        </div>
+        <div className='gap-2'>{children}</div>
+        <div className='flex gap-2 flex-1'></div>
       </div>
     </div>
   );
