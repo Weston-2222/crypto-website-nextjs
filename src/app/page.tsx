a97eaacddf309ae684f17bcb203e0377192561fb
weston-2222 <westonxie22@gmail.com>
2024-12-23 07:40:19 +0800

    feat: 新增控制參數dialog的參數

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,30 @@ import 'server-only';
 import React from 'react';
 import CoinMasketsCapTable from '../components/coinMasketsCapTable';
 import { getCoinMarketsData } from '@/services/coinGecko/coinMarkets';
+import LoginDialog from '@/components/loginDialog';
 
 export const revalidate = 3600; // 每小時重新生成靜態頁面（3600 秒）
 
-const Page = async () => {
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: { isOpenLoginDialog: string };
+}) => {
   try {
+    const params = await searchParams;
     const data = await getCoinMarketsData();
 
     return (
-      <div className='container mx-auto p-0'>
-        <h1 className='text-3xl p-5'>市值排名</h1>
-        <CoinMasketsCapTable data={data} />
-      </div>
+      <>
+        <LoginDialog
+          defaultOpen={params.isOpenLoginDialog == 'true'}
+          isIcon={false}
+        />
+        <div className='container mx-auto p-0 bg-gray-100 dark:bg-black'>
+          <h1 className='text-3xl p-5'>市值排名</h1>
+          <CoinMasketsCapTable data={data} />
+        </div>
+      </>
     );
   } catch {
     return (
