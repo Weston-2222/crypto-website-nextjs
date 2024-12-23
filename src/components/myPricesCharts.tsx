18e7153683ed79dcc2028d6c50b2eb06fcfaa525
weston-2222 <westonxie22@gmail.com>
2024-12-23 08:45:06 +0800

    fix: 刪除無用的資訊

--- src/components/myPricesCharts.tsx
+++ src/components/myPricesCharts.tsx
@@ -2,14 +2,12 @@
 import { useEffect, useState, useCallback } from 'react';
 import { Tabs, TabsList, TabsTrigger } from './ui/tabs';
 
-import { TrendingUp } from 'lucide-react';
 import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts';
 
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
@@ -206,14 +204,14 @@ const MyPricesCharts = ({
               </LineChart>
             </ChartContainer>
           </CardContent>
-          <CardFooter className='flex-col items-start gap-2 text-sm'>
+          {/* <CardFooter className='flex-col items-start gap-2 text-sm'>
             <div className='flex gap-2 font-medium leading-none'>
               Trending up by 5.2% this month <TrendingUp className='h-4 w-4' />
             </div>
             <div className='leading-none text-muted-foreground'>
               Showing total visitors for the last 6 months
             </div>
-          </CardFooter>
+          </CardFooter> */}
         </Card>
       </Tabs>
     </>
