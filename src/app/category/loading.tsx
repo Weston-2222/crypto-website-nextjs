2d955cb9b9c9e4b014b87ecc286b413dc446ae8a
weston-2222 <westonxie22@gmail.com>
2025-01-08 06:23:30 +0800

    feat: 把全部路由的loading統一

--- src/app/category/loading.tsx
+++
@@ -1,9 +0,0 @@
-const Loading = () => {
-  return (
-    <div className='flex justify-center items-center h-screen'>
-      <div className='loading-spinner w-20 h-20'></div>
-    </div>
-  );
-};
-
-export default Loading;
