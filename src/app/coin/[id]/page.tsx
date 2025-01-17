eef78d368f79746ea4b3e14535fa0066df4a8d26
weston-2222 <westonxie22@gmail.com>
2025-01-17 19:14:34 +0800

    fix: 將Suspense拿掉

--- src/app/coin/[id]/page.tsx
+++ src/app/coin/[id]/page.tsx
@@ -1,8 +1,6 @@
 import 'server-only';
 import CoinTitle from './components/coinTitle';
-import { Suspense } from 'react';
 import MarketInfo from './components/marketInfo';
-import Loading from '@/components/loading';
 import LinkInfo from './components/linkInfo';
 import DeveloperInfo from './components/developerInfo';
 import CategoryInfo from './components/categoryInfo';
@@ -15,57 +13,28 @@ const CoinPage = async ({ params }: { params: Promise<{ id: string }> }) => {
     <>
       <div className='w-full'>
         <div>
-          <Suspense
-            fallback={
-              <Loading
-                loadingSize='w-20 h-20'
-                className='h-[100px] w-[400px]'
-              />
-            }
-          >
-            <CoinTitle id={id} />
-          </Suspense>
+          <CoinTitle id={id} />
         </div>
       </div>
       <div className='flex flex-col xl:flex-row lg:space-x-4 space-y-4 xl:space-y-0 gap-4'>
         {/* 在中小螢幕時 CoinPriceTabs 在上面，正常電腦螢幕時 CoinPriceTabs 在右邊 */}
         <div className='xl:order-2 w-full xl:w-2/3'>
-          <Suspense
-            fallback={<Loading className='h-[600px]' loadingSize='w-20 h-20' />}
-          >
-            <PriceChart id={id} />
-          </Suspense>
+          <PriceChart id={id} />
 
           {/* 市場資料 */}
-          <Suspense
-            fallback={<Loading className='h-1/2' loadingSize='w-20 h-20' />}
-          >
-            <CategoryInfo id={id} />
-          </Suspense>
+          <CategoryInfo id={id} />
         </div>
 
         {/* 在中小螢幕時 CoinBasicInfoCard 在下面，正常電腦螢幕時 CoinBasicInfoCard 在左邊 */}
         <div className='xl:order-1 w-full xl:w-1/3'>
           {/* 市場資料 */}
-          <Suspense
-            fallback={<Loading className='h-[500px]' loadingSize='w-20 h-20' />}
-          >
-            <MarketInfo id={id} />
-          </Suspense>
+          <MarketInfo id={id} />
 
           {/* 鏈接資料 */}
-          <Suspense
-            fallback={<Loading className='h-1/2' loadingSize='w-20 h-20' />}
-          >
-            <LinkInfo id={id} />
-          </Suspense>
+          <LinkInfo id={id} />
 
           {/* 開發者資料 */}
-          <Suspense
-            fallback={<Loading className='h-1/2' loadingSize='w-20 h-20' />}
-          >
-            <DeveloperInfo id={id} />
-          </Suspense>
+          <DeveloperInfo id={id} />
         </div>
       </div>
     </>
