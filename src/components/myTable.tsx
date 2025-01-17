291c3c35be60fd0c8d61a95b562b1543e542f788
weston-2222 <westonxie22@gmail.com>
2025-01-17 20:00:23 +0800

    fix: 修正樣式

--- src/components/myTable.tsx
+++ src/components/myTable.tsx
@@ -75,7 +75,7 @@ export default function MyTable<T>({
               <TableRow
                 key={row.id}
                 data-state={row.getIsSelected() && 'selected'}
-                className='cursor-pointer lg:hover:bg-gray-200 lg:dark:hover:bg-gray-800'
+                className='cursor-pointer lg:hover:bg-gray-200 lg:dark:hover:bg-gray-800 hover:null'
                 onClick={() => onRowClick?.(row.original)}
               >
                 {row.getVisibleCells().map((cell, index) => (
